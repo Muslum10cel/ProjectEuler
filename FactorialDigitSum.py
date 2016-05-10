2d6beb009c9f35aa698bbb9287fc3093498dd3ac
muslumoncel <muslumoncel10@gmail.com>
2016-05-10 03:54:02 +0300

    commit

---
+++ FactorialDigitSum.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+sys.setrecursionlimit(1500)
+T = int(input())
+while T > 0:
+    result = fact(int(input()))
+    s = 0
+    while result != 0:
+        try:
+            s += (result % 10)
+        except:
+            pass
+        result //= 10
+    print(s)
+    T -= 1
